3c8965feb4d5ed2597ed5322ea0bdbcc015dc31c
sourabh1510 <sourabh1510@gmail.com>
2014-04-16 21:37:20 -0400

    Create ChatStatistics.java
    Prints the total number of lines in the chat
    Prints the total number of messages in the chat. A message with multiple lines is counted as one.
    Prints the Message count for each group member

---
+++ sv/chat/stat/ChatStatistics.java
@@ -0,0 +1,71 @@
+package sv.chat.stat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class ChatStatistics {
+
+	public static void main(String[] args) {
+	
+		try {
+			int count=0;
+			int lineCount=0;
+			BufferedReader br = new BufferedReader(new FileReader("fullChat_Apr15_2014_2333.txt"));
+
+			Map<String, Integer> messagePerHeadCount=new HashMap();
+			String line = br.readLine();
+
+			while (line != null) {
+				lineCount++;
+
+				System.out.println(line);
+				if(line.indexOf(" - ")==13){
+					count++;
+					String date=line.substring(0, line.indexOf(" - "));
+					String name =line.substring(16, line.indexOf(":", 16));
+
+					if(messagePerHeadCount.containsKey(name)){
+						messagePerHeadCount.put(name, (messagePerHeadCount.get(name)+1));
+					}else{
+						messagePerHeadCount.put(name, 0);
+					}
+
+					//System.out.println(date);
+					// System.out.println(name);
+				}else{
+
+				}
+				line = br.readLine();
+
+			}
+
+			System.out.println("====================================\n"+"Line Count:"+lineCount);
+			System.out.println("====================================\n"+"Message Count:"+count);
+			System.out.println("====================================\n");
+		
+
+			String entrySet=messagePerHeadCount.entrySet().toString();
+			StringTokenizer tokenizer=new StringTokenizer(entrySet,",");
+
+			while(tokenizer.hasMoreElements()){
+				System.out.println(tokenizer.nextElement());
+			}
+			System.out.println("====================================\n");
+
+			br.close();
+		} catch(Exception e){
+			e.printStackTrace();
+
+		}
+
+
+	}
+
+}
